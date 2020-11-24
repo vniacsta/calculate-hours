75005d39fa4aeddc1b50b8c5ac2cca7429a80c83
Vania Costa <vniacsta@users.noreply.github.com>
2020-11-24 19:08:56 +0000

    Changed if else to if statements, Changed intro text and removed media queries

--- js/app.js
+++ js/app.js
@@ -42,8 +42,9 @@ calculate = (number) => {
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${days} dias e ${hours} horas`;
+  } 
   
-  } else if (selectedOptionBox1 === 'hours' && selectedOptionBox2 === 'months') {
+  if (selectedOptionBox1 === 'hours' && selectedOptionBox2 === 'months') {
     let hoursToMonths = number / 720;
     let months = Math.floor(hoursToMonths);
     let monthsDecimals = (hoursToMonths % 1);
@@ -55,8 +56,9 @@ calculate = (number) => {
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${months} meses, ${days} dias e ${hours} horas`;    
+  } 
   
-  } else if (selectedOptionBox1 === 'hours' && selectedOptionBox2 === 'years') {
+  if (selectedOptionBox1 === 'hours' && selectedOptionBox2 === 'years') {
     let hoursToYears = number / 8760;
     let years = Math.floor(hoursToYears);
     let yearsDecimals = (hoursToYears % 1);
@@ -71,14 +73,16 @@ calculate = (number) => {
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${years} anos, ${months} meses, ${days} dias e ${hours} horas`;    
+  }
   
-  } else if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'hours') {
+  if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'hours') {
     let hours = number * 24;
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${hours} horas`;
+  } 
   
-  } else if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'months') {
+  if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'months') {
     let daysToMonths = number / 30;
     let months = Math.floor(daysToMonths);
     let monthsDecimals = (daysToMonths % 1);
@@ -87,8 +91,9 @@ calculate = (number) => {
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${months} meses e ${days} dias`;
-
-  } else if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'years') {
+  } 
+  
+  if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'years') {
     let daysToYears = number / 365;
     let years = Math.floor(daysToYears);
     let yearsDecimals = (daysToYears % 1);
@@ -100,20 +105,23 @@ calculate = (number) => {
 
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${years} anos, ${months} meses e ${days} dias`;    
+  } 
   
-  } else if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'hours') {
+  if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'hours') {
     let hours = number * 720;
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${hours} horas`;    
-
-  } else if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'days') {
+  } 
+  
+  if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'days') {
     let days = number * 30;
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${days} dias`;    
-    
-  } else if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'years') {
+  } 
+  
+  if (selectedOptionBox1 === 'months' && selectedOptionBox2 === 'years') {
     let monthsToYears = number / 12;
     let years = Math.floor(monthsToYears);
     let yearsDecimals = (monthsToYears % 1);
@@ -122,31 +130,53 @@ calculate = (number) => {
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${years} anos e ${months} meses`;
-    
-  } else if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'hours') {
+  } 
+  
+  if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'hours') {
     let hours = number * 24;
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${hours} horas`;
-    
-  } else if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'days') {
+  } 
+  
+  if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'days') {
     let days = number * 365;
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${days} dias`;
-    
-  } else if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'months') {
+  } 
+  
+  if (selectedOptionBox1 === 'years' && selectedOptionBox2 === 'months') {
     let months = number * 12;
     
     resultBox.setAttribute('style', 'display: inherit');
     output.innerText = `${months} meses`;
   
   } 
+  
+  /*if (selectedOptionBox1 === 'hours' && selectedOptionBox2 === 'working') {
+    let working = number / 40;
+    let weeks = Math.floor(working);
+    let days = (working % 1);
+    
+    // 1200 hours / 8h dia = 150 dias / 5 dias por semana = 30 semanas * 4 semanas por mês = 7.5 meses
+    // 1200 hours / (8h dia * 5 dias semana = 40 horas por semana)
+
+    resultBox.setAttribute('style', 'display: inherit');
+    output.innerHTML = `${weeks} semanas and ${days} dias`;
+
+  } else if (selectedOptionBox1 === 'days' && selectedOptionBox2 === 'working') {
+    let days = number / 40;
+    let days = number / 8;
+    
+    resultBox.setAttribute('style', 'display: inherit');
+    output.innerHTML = `${weeks} semanas | ${days} dias`;
+  }*/
 }
 
-$(document).bind('pageinit', function(){
-  $(submit).vclick(function() {
-    input = document.querySelector('#input-number');
-    calculate(input.value);
-   });
-});
+submit.onclick = (event) => {
+  event.preventDefault();
+  
+  input = document.querySelector('#input-number');
+  calculate(input.value);
+};
